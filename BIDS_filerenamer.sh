eb0ce8ba65434908910eae0c6170b50f9b275bf9
Vivian Roger Steiger <vivian@dhcp-wlan-uzh-10-12-5-044.uzh.ch>
2017-03-28 15:30:48 +0200

    BIDS_helper scripts adapted to handle sessions

--- BIDS_filerenamer.sh
+++ BIDS_filerenamer.sh
@@ -6,16 +6,24 @@ echo -n "Enter the name of the folder and press [ENTER]: "
 read foldername
 
 echo
+echo -n "Enter the number of sessions and press [ENTER]: "
+read sessions
+
+echo
 echo -n "Enter the number of subjects and press [ENTER]: "
 read subs
 
-echo "Renaming files according to BIDS Reference for Project $foldername "
+echo "Renaming files according to BIDS Reference for Project $foldername with $subs Subjects and $sessions Sessions"
 
 cd $foldername
 
 f_subj=1
 s_subj=9
 l_subj=$subs
+
+f_sess=1
+s_sess=9
+l_sess=$sessions
 
 for i in $(eval echo "{$f_subj..$l_subj}")
 do
@@ -31,5 +39,45 @@ do
     mv sub-${i}/dwi/*.bval sub-${i}/dwi/sub-${i}_dwi.bval
     mv sub-${i}/dwi/*.bvec sub-${i}/dwi/sub-${i}dwi.bvec
     mv sub-${i}/func/*.nii.gz sub-${i}/func/sub-${i}_task-rest_bold.nii.gz
+  fi
+done
+
+
+for i in $(eval echo "{$f_subj..$l_subj}")
+do
+  if [ $i -le $s_subj ]; then
+    for j in $(eval echo "{$f_sess..$l_sess}")
+    do
+      if [ $j -le $s_sess ]; then
+        mv sub-0${i}/ses-0${j}/anat/*.nii.gz sub-0${i}/ses-0${j}/anat/sub-0${i}_ses-0${j}_T1w.nii.gz
+        mv sub-0${i}/ses-0${j}/dwi/*.nii.gz sub-0${i}/ses-0${j}/dwi/sub-0${i}_ses-0${j}_dwi.nii.gz
+        mv sub-0${i}/ses-0${j}/dwi/*.bval sub-0${i}/ses-0${j}/dwi/sub-0${i}_ses-0${j}_dwi.bval
+        mv sub-0${i}/ses-0${j}/dwi/*.bvec sub-0${i}/ses-0${j}/dwi/sub-0${i}_ses-0${j}_dwi.bvec
+        mv sub-0${i}/ses-0${j}/func/*.nii.gz sub-0${i}/ses-0${j}/func/sub-0${i}_ses-0${j}_task-rest_bold.nii.gz
+      else
+        mv sub-0${i}/ses-${j}/anat/*.nii.gz sub-0${i}/ses-${j}/anat/sub-0${i}_ses-${j}_T1w.nii.gz
+        mv sub-0${i}/ses-${j}/dwi/*.nii.gz sub-0${i}/ses-${j}/dwi/sub-0${i}_ses-${j}_dwi.nii.gz
+        mv sub-0${i}/ses-${j}/dwi/*.bval sub-0${i}/ses-${j}/dwi/sub-0${i}_ses-${j}_dwi.bval
+        mv sub-0${i}/ses-${j}/dwi/*.bvec sub-0${i}/ses-${j}/dwi/sub-0${i}_ses-${j}_dwi.bvec
+        mv sub-0${i}/ses-${j}/func/*.nii.gz sub-0${i}/ses-${j}/func/sub-0${i}_ses-${j}_task-rest_bold.nii.gz
+      fi
+    done
+  else
+    for j in $(eval echo "{$f_sess..$l_sess}")
+    do
+      if [ $j -le $s_sess ]; then
+        mv sub-${i}/ses-0${j}/anat/*.nii.gz sub-${i}/ses-0${j}/anat/sub-${i}_ses-0${j}_T1w.nii.gz
+        mv sub-${i}/ses-0${j}/dwi/*.nii.gz sub-${i}/ses-0${j}/dwi/sub-${i}_ses-0${j}_dwi.nii.gz
+        mv sub-${i}/ses-0${j}/dwi/*.bval sub-${i}/ses-0${j}/dwi/sub-${i}_ses-0${j}_dwi.bval
+        mv sub-${i}/ses-0${j}/dwi/*.bvec sub-${i}/ses-0${j}/dwi/sub-${i}_ses-0${j}_dwi.bvec
+        mv sub-${i}/ses-0${j}/func/*.nii.gz sub-${i}/ses-0${j}/func/sub-${i}_ses-0${j}_task-rest_bold.nii.gz
+      else
+        mv sub-${i}/ses-${j}/anat/*.nii.gz sub-${i}/ses-${j}/anat/sub-${i}_ses-${j}_T1w.nii.gz
+        mv sub-${i}/ses-${j}/dwi/*.nii.gz sub-${i}/ses-${j}/dwi/sub-${i}_ses-${j}_dwi.nii.gz
+        mv sub-${i}/ses-${j}/dwi/*.bval sub-${i}/ses-${j}/dwi/sub-${i}_ses-${j}_dwi.bval
+        mv sub-${i}/ses-${j}/dwi/*.bvec sub-${i}/ses-${j}/dwi/sub-${i}_ses-${j}_dwi.bvec
+        mv sub-${i}/ses-${j}/func/*.nii.gz sub-${i}/ses-${j}/func/sub-${i}_ses-${j}_task-rest_bold.nii.gz
+      fi
+    done
   fi
 done
--- BIDS_folders.sh
+++ BIDS_folders.sh
@@ -6,10 +6,14 @@ echo -n "Enter the name of the folder and press [ENTER]: "
 read foldername
 
 echo
+echo -n "Enter the number of sessions and press [ENTER]: "
+read sessions
+
+echo
 echo -n "Enter the number of subjects and press [ENTER]: "
 read subs
 
-echo "Building BIDS Folder structure in $foldername "
+echo "Renaming files according to BIDS Reference for Project $foldername with $subs Subjects and $sessions Sessions"
 
 mkdir $foldername
 cd $foldername
@@ -18,11 +22,31 @@ f_subj=1
 s_subj=9
 l_subj=$subs
 
+f_sess=1
+s_sess=9
+l_sess=$sessions
+
+
+
 for i in $(eval echo "{$f_subj..$l_subj}")
 do
   if [ $i -le $s_subj ]; then
-    mkdir -p sub-0${i}/{anat,dwi,func}
+    for j in $(eval echo "{$f_sess..$l_sess}")
+    do
+      if [ $j -le $s_sess ]; then
+        mkdir -p sub-0${i}/ses-0$j/{anat,dwi,func}
+      else
+        mkdir -p sub-0${i}/ses-$j/{anat,dwi,func}
+      fi
+    done
   else
-    mkdir -p sub-${i}/{anat,dwi,func}
+    for j in $(eval echo "{$f_sess..$l_sess}")
+    do
+      if [ $j -le $s_sess ]; then
+        mkdir -p sub-${i}/ses-0$j/{anat,dwi,func}
+      else
+        mkdir -p sub-${i}/ses-$j/{anat,dwi,func}
+      fi
+    done  
   fi
 done
